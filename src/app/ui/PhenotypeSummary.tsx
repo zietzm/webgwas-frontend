d0b1c0a7c2bf51a6f2cd191845df0aed84ae7222
zietzm <michael.zietz@gmail.com>
2024-09-03 11:58:36 -0700

    chore: improve documentation for phenotype fit quality summary

--- src/app/ui/PhenotypeSummary.tsx
+++ src/app/ui/PhenotypeSummary.tsx
@@ -21,12 +21,15 @@ export function SummaryDocumentation() {
   return (
     <div className="p-5">
       <p className="mb-2 text-gray-600 dark:text-gray-400">
-        WebGWAS approximates the phenotype you built using a linear regression
-        against all other phenotypes. The left figure compares the true and
-        approximated phenotype values. The quality of this approximation
-        determines the fidelity of the GWAS. The right plot shows the
-        relationship between phenotype fits and GWAS summary statistics. The red
-        line shows the fit quality of the phenotype you defined.
+        The left figure compares the true and approximated phenotype values. The
+        quality of this approximation determines the fidelity of the GWAS. The
+        right plot shows the relationship between phenotype fits and GWAS
+        summary statistics. The red line shows the fit quality of the phenotype
+        you defined.
+      </p>
+      <p className="mb-2 text-gray-600 dark:text-gray-400">
+        Note: No point in the phenotype plot has fewer than 10 data points. This
+        is because we anonymized the data using k-anonymity with k=10.
       </p>
     </div>
   );
