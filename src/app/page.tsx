b4b37886691ec555d5a17265fb32be87a52a50dd
zietzm <michael.zietz@gmail.com>
2024-10-01 10:08:34 -0700

    fix: simplify accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { useState } from "react";
 import SimplePhenotypeBuilder, {
   simplePhenotypeBuilderUsage,
 } from "@/app/ui/SimplePhenotype";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
 function headerInformation() {
   return (
@@ -36,42 +38,25 @@ function headerInformation() {
 }
 
 function accordionUsage() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div id="accordion-collapse" data-accordion="collapse">
-      <h2 id="accordion-collapse-heading-3">
+    <div>
+      <h2>
         <button
           type="button"
-          className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
-          data-accordion-target="#accordion-collapse-body-3"
-          aria-expanded="false"
-          aria-controls="accordion-collapse-body-3"
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center justify-between w-full p-5 font-medium rtl:text-right bg-gray-100 text-gray-500 border border-gray-200 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-800 gap-3"
         >
           <span>Usage Instructions</span>
-          <svg
-            data-accordion-icon
-            className="w-3 h-3 rotate-180 shrink-0"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 6"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M9 5 5 1 1 5"
-            />
-          </svg>
+          {isOpen ? (
+            <ChevronUp className="font-bold w-5 h-5 shrink-0" />
+          ) : (
+            <ChevronDown className="font-bold w-5 h-5 shrink-0" />
+          )}
         </button>
       </h2>
-      <div
-        id="accordion-collapse-body-3"
-        className="hidden"
-        aria-labelledby="accordion-collapse-heading-3"
-      >
-        {simplePhenotypeBuilderUsage()}
-      </div>
+      {isOpen && simplePhenotypeBuilderUsage()}
     </div>
   );
 }
