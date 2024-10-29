328f4e43cc408884a2d0ae5484abe75d6e91499e
zietzm <michael.zietz@gmail.com>
2024-10-29 11:58:54 -0700

    feat: add logo to header and fix spacing/width

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Menu, X } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 
@@ -27,65 +28,77 @@ export default function Header() {
 
   return (
     <header className="bg-blue-main text-white relative">
-      <div className="container max-w-7xl mx-auto top-0">
-        <div className="mx-auto px-4 py-6 flex justify-between items-center">
-          <Link href="/" className="text-2xl font-bold">
-            WebGWAS
-          </Link>
-          {/* Desktop navigation */}
-          <div className="hidden md:flex">
-            <nav className="space-x-4 py-4">
-              {sections.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  className={`px-3 py-2 rounded-md text-lg ${
-                    pathname === item.href
-                      ? "bg-white text-blue-main hover:text-gray-900"
-                      : "text-white hover:bg-blue-dark"
-                  }`}
-                >
-                  {item.name}
-                </Link>
-              ))}
-            </nav>
-          </div>
-
-          <div className="md:hidden flex items-center justify-between relative">
-            {/* Menu/X Button */}
-            <Button
-              variant="ghost"
-              size="icon"
-              onClick={() => setOpen(!open)}
-              className="z-20 mx-2"
+      <div className="container mx-auto max-w-4xl top-0 px-4">
+        <div className="container mx-auto flex-1">
+          <div className="mx-auto py-6 flex justify-between items-center">
+            <Link
+              href="/"
+              className="flex items-center gap-2 text-2xl font-bold"
             >
-              {open ? (
-                <X className="h-8 w-8 text-sky-800" />
-              ) : (
-                <Menu className="h-8 w-8 text-sky-800" />
-              )}
-            </Button>
+              <Image
+                src="/logo.webp"
+                alt="WebGWAS logo"
+                width={50}
+                height={50}
+                unoptimized
+              />
+              WebGWAS
+            </Link>
+            {/* Desktop navigation */}
+            <div className="hidden md:flex">
+              <nav className="space-x-4 py-4">
+                {sections.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className={`px-3 py-2 rounded-md text-lg ${
+                      pathname === item.href
+                        ? "bg-white text-blue-main hover:text-gray-900"
+                        : "text-white hover:bg-blue-dark"
+                    }`}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+
+            <div className="md:hidden flex items-center justify-between relative">
+              {/* Menu/X Button */}
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={() => setOpen(!open)}
+                className="z-20 mx-2"
+              >
+                {open ? (
+                  <X className="h-8 w-8 text-sky-800" />
+                ) : (
+                  <Menu className="h-8 w-8 text-sky-800" />
+                )}
+              </Button>
+            </div>
           </div>
+          {/* Navigation Sheet */}
+          {open && (
+            <div className="md:hidden absolute top-full left-0 w-full bg-white z-10 shadow-md">
+              <nav className="flex flex-col space-y-4 p-4">
+                {sections.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className={`text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-md font-medium ${
+                      pathname === item.href ? "bg-gray-100" : ""
+                    }`}
+                    onClick={() => setOpen(false)}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+          )}
         </div>
-        {/* Navigation Sheet */}
-        {open && (
-          <div className="md:hidden absolute top-full left-0 w-full bg-white z-10 shadow-md">
-            <nav className="flex flex-col space-y-4 p-4">
-              {sections.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  className={`text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-md font-medium ${
-                    pathname === item.href ? "bg-gray-100" : ""
-                  }`}
-                  onClick={() => setOpen(false)}
-                >
-                  {item.name}
-                </Link>
-              ))}
-            </nav>
-          </div>
-        )}
       </div>
     </header>
   );
