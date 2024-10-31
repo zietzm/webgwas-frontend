2c9a1e34b55dcda2e136c3b454cbf5ad429e66d4
zietzm <michael.zietz@gmail.com>
2024-10-30 17:12:12 -0700

    fix: footer too wide

--- src/app/ui/Footer.tsx
+++ src/app/ui/Footer.tsx
@@ -4,7 +4,7 @@ import { Github, TLab } from "./Icons";
 const Footer = () => {
   return (
     <footer className="bg-gray-100 text-gray-600 py-8">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <p className="text-sm">
